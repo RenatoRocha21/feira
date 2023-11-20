0d1b3022167395bd0dd62c4c3c2dff954456db9c
RenatoRocha21 <149381147+RenatoRocha21@users.noreply.github.com>
2023-11-20 14:24:10 -0300

    Add files via upload

--- home.js
+++ home.js
@@ -17,6 +17,54 @@ function harley() {
     as(pops, imgs, sts);
 }
 
+function ney() {
+    var pops = "ney-pop";
+    var imgs = "ney-prf";
+    var sts = "st-hn";
+    as(pops, imgs, sts);
+}
+
+function bonny() {
+    var pops = "bonny-pop";
+    var imgs = "bonny-prf";
+    var sts = "st-bon";
+    as(pops, imgs, sts);
+}
+
+function rob() {
+    var pops = "rob-pop";
+    var imgs = "rob-prf";
+    var sts = "st-rob";
+    as(pops, imgs, sts);
+}
+
+function yur() {
+    var pops = "yur-pop";
+    var imgs = "yur-prf";
+    var sts = "st-yur";
+    as(pops, imgs, sts);
+}
+
+function jao() {
+    var pops = "jao-pop";
+    var imgs = "jao-prf";
+    var sts = "st-jao";
+    as(pops, imgs, sts);
+}
+
+function nusta() {
+    var pops = "nusta-pop";
+    var imgs = "nusta-prf";
+    var sts = "st-nusta";
+    as(pops, imgs, sts);
+}
+function ale() {
+    var pops = "ale-pop";
+    var imgs = "ale-prf";
+    var sts = "st-ale";
+    as(pops, imgs, sts);
+}
+
 function as(pops, imgs, sts) {
     var idmg = document.getElementById(imgs);
     var idiv = document.getElementById(pops);
